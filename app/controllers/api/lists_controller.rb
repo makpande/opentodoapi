90e5d3b30f4165d414face09dff28c9541639ecc
Makarand Pande <mak_pande@yahoo.com>
2015-11-20 08:04:36 -0500

    project complete

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,56 +1,47 @@
 class Api::ListsController < ApiController
    before_action :authenticated?
    before_action :set_user
+
    def show
      @list = List.all
-     render json: @list
+     render json: @list, each_serializer: ListSerializer
    end
-    def create
-      @list = @user.lists.new( list_params )
 
-      if @list.save
-        render json: { success: "list sucessfully created" }, status: :created
-      else
-        render json: { failure: "invalid name" }, status: :unprocessable_entity
-      end
-    end
+   def create
+     @list = @user.lists.new( list_params )
 
-    def destroy
-      begin
-        @list = @user.lists.find(params[:id])
-        @list.destroy
-        render json: {}, status: :no_content
-        rescue ActiveRecord::RecordNotFound
-        render :json => {}, :status => :not_found
-      end
-    end
+     if @list.save
+       render json: { success: "list sucessfully created" }, status: :created
+     else
+       render json: { failure: "invalid name" }, status: :unprocessable_entity
+     end
+   end
 
-    def update
-      @list = @user.lists.find(params[:id])
-      if @list.update( list_params )
-        render json: @list
-      else
-        render json: { errors: @list.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
+   def destroy
+     begin
+       @list = @user.lists.find(params[:id])
+       @list.destroy
+       render json: {}, status: :no_content
+       rescue ActiveRecord::RecordNotFound
+       render :json => {}, :status => :not_found
+     end
+   end
+
+   def update
+     @list = @user.lists.find(params[:id])
+     if @list.update( list_params )
+       render json: @list
+     else
+       render json: { errors: @list.errors.full_messages }, status: :unprocessable_entity
+     end
+   end
 
   private
 
   def list_params
     params.require( :list ).permit( :name, :permissions )
   end
-
   def set_user
     @user = User.find(params[:user_id])
   end
 end
-   #
-  #  def update
-  #    list = List.find(params[:id])
-   #
-  #    if list.update(list_params)
-  #      render json: list
-  #    else
-  #      render json: { errors: list.errors.full_messages }, status: :unprocessable_entity
-  #    end
-  #  end

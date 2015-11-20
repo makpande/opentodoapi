6af2a79d4a70f6c4dc8f1ecb9e68f4c42147666a
Makarand Pande <mak_pande@yahoo.com>
2015-11-19 22:06:35 -0500

    destroy list finished

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,11 +3,31 @@ class Api::UsersController < ApiController
   before_action :authenticated?
 
   def index
-
     return permission_denied_error unless condition_met
     users = User.all
     render json: users, each_serializer: UserSerializer
   end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+     begin
+       user = User.find(params[:id])
+       user.destroy
+
+       render json: {}, status: :no_content
+     rescue ActiveRecord::RecordNotFound
+       render :json => {}, :status => :not_found
+     end
+   end
 
   private
 
